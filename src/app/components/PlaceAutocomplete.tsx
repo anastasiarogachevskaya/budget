77a29bd3cd48f8b2158c255f98f357827570aa30
anastasia rogachevskaya <anastasia.rogachevskaya@relexsolutions.com>
2024-04-27 17:08:23 +0300

    Simplification

--- src/app/components/PlaceAutocomplete.tsx
+++ src/app/components/PlaceAutocomplete.tsx
@@ -15,7 +15,6 @@ interface PlaceAutocompleteProps {
 
 function PlaceAutocomplete({ formData, setFormData }: PlaceAutocompleteProps) {
   const [places, setPlaces] = useState<Place[]>([]);
-  const [value, setValue] = useState<Place | null>(null);
 
   useEffect(() => {
     fetch('/api/places')
@@ -26,12 +25,8 @@ function PlaceAutocomplete({ formData, setFormData }: PlaceAutocompleteProps) {
 
   return (
     <Autocomplete
-      value={value ? value.name : null}
       onChange={(event, newValue: Place | string | null) => {
         if (typeof newValue === 'string') {
-          setValue({
-            name: newValue,
-          });
           setFormData({ ...formData, place: newValue });
         } else if (newValue && newValue.inputValue) {
           fetch('/api/places', {
@@ -43,16 +38,11 @@ function PlaceAutocomplete({ formData, setFormData }: PlaceAutocompleteProps) {
           })
             .then((res) => res.json())
             .then((data) => {
-              // Update the places state with the new place
-              setValue({
-                name: newValue.inputValue || 'Default Name',
-              });
               setFormData({ ...formData, place: newValue.inputValue });
               setPlaces([...places, data]);
             })
             .catch(console.error);
         } else {
-          setValue(null);
           setFormData({ ...formData, place: newValue?.name || '' });
         }
       }}
@@ -60,7 +50,6 @@ function PlaceAutocomplete({ formData, setFormData }: PlaceAutocompleteProps) {
         const filtered = filter(options, params);
 
         const { inputValue } = params;
-        // Suggest the creation of a new value
         const isExisting = options.some((option: Place | string) => {
           if (typeof option === 'string') {
             return inputValue === option;
@@ -78,15 +67,12 @@ function PlaceAutocomplete({ formData, setFormData }: PlaceAutocompleteProps) {
       }}
       options={places.map((place) => place.name)}
       getOptionLabel={(option) => {
-        // Value selected with enter, right from the input
         if (typeof option === 'string') {
           return option;
         }
-        // Add "xxx" option created dynamically
         if (option.inputValue) {
           return option.inputValue;
         }
-        // Regular option
         return option.name;
       }}
       renderOption={(props, option: Place | string) => <li {...props}>{typeof option === 'string' ? option : option.name}</li>}
